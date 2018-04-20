1f841e17920e5538abaf94e0428afc9b1fad6018
hankhyd <hankhyd@gmail.com>
2018-04-19 20:02:39 -0400

    testing

--- tensorflow1.py
+++ tensorflow1.py
@@ -1,28 +1,33 @@
-import random
-from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
+import numpy as np
 
-FLAGS = None
-mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+#creat data
+x_data = np.random.rand(100).astype(np.float32)
+y_data = x_data*0.3 + 0.1
 
-x = tf.placeholder(tf.float32, [None, 784])
-W = tf.Variable(tf.zeros([784, 10]))
-b = tf. Variable(tf.zeros([10]))
-y = tf. matmul(x, W) + b
-y_ = tf.placeholder(tf.float32, [None, 10])
+#tensorflow structure
+Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+Bias = tf.Variable(tf.zeros([1]))
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y_, logits = y))
-train_step = tf.train.GradientDescentOptimizer(0.4).minimize(cross_entropy)
+y = Weights*x_data + Bias
+
+loss = tf.reduce_mean(tf.square(y - y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
 
 init = tf.initialize_all_variables()
 
+
+#initialize
 sess = tf.Session()
 sess.run(init)
 
-for i in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+for step in range(500):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(Weights), sess.run(Bias))
+
+
+sate = tf.Variable()
