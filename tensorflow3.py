1eb150596e7693706b1cbffa0d2fa7c8658b7be4
hankhyd <hankhyd@gmail.com>
2018-04-23 20:24:49 -0400

    added some instruction for tf.placeholder, feed_dict, np.linspace as well as tf.reduce_sum

--- tensorflow3.py
+++ tensorflow3.py
@@ -11,24 +11,22 @@ def layer(inputs, row, column, activation_function = None):
         outputs = activation_function(func)
     return outputs
 
+# creating some fake data
 x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
-            # np.linspace(start, end, num=, endpoint=true, restep=False, dtype=None)
-            # Will return evenly spaced numbers over a specified interval
-            # clearly, the parameters of start and end defining the specified interval
-            # num means the number of samples will be generate
-            # for np.newaxis is tying to conver a 1-D vector into either row vectro or column vector
-            # [:, np.newaxis] will convert a 1-D vector into a column vector
-            # [np.newaxis, :] will convert a 1-D vector into a row vector
-noise = np.random.normal(0, 0.05, x_data.shape)
+noise = np.random.normal(0, 0.05, x_data.shape)# I'm creating fake data close to reality by adding some noise with it
 y_data = np.square(x_data) - 0.5 + noise
-            # the reason I defined a noise here is becasue i am trying to create a dataset that is more close to reality
+
+# input training data x and y
 xs = tf.placeholder(tf.float32, [None, 1])
 ys = tf.placeholder(tf.float32, [None, 1])
 
+# Building Model
+# There're only 3 layers in this simple model, the model structure is looking like 1-10-1,
+# where the numbers mean how many neurons in each layer
 layer_1 = layer(xs, 1, 10, activation_function = tf.nn.relu)
 outputs = layer(layer_1, 10, 1, activation_function = None)
 
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - outputs), reduction_indices=[1]))
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - outputs), axis=[1]))
 train = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 
 init = tf.global_variables_initializer()
@@ -39,5 +37,21 @@ sess.run(init)
 for i in range (1000):
     sess.run(train, feed_dict={xs: x_data, ys: y_data})
     if i % 100 == 0:
-        print(i, sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+        print(i, sess.run(loss, feed_dict={xs: x_data, ys: y_data})) # since i'm using placeholder for input data,
+                                                                     # but the way placeholder input data is by using feed_dict
+                                                                     # and its a dictionary, that's why i am using {}
 
+
+# np.linspace(start, end, num=, endpoint=true, restep=False, dtype=None)
+# will return evenly spaced numbers over a specified interval
+# clearly, the parameters of start and end defining the specified interval
+# num means the number of samples will be generate
+# for np.newaxis is tying to conver a 1-D vector into either row vector or column vector
+# [:, np.newaxis] will convert a 1-D vector into a column vector
+# [np.newaxis, :] will convert a 1-D vector into a row vector
+
+
+# tf.reduce_sum(input_tensor, axis, keepdims, name)
+# is trying to first of all, compute sum of elements across dimensions of a tensor
+# then it will reduce the dimension of input_tensor along the dimensions given in axis. Unless keepdims is true, the rank of tensor is reduced by 1 for each entry in axis.
+# if keepdims is true, the returned value will maintain the same dimension as the original input_tensor
